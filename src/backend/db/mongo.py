99e1a897422fb74a436f516b7cb5b05f5edd78fa
Ramzey Alissa <mramzey@ymail.com>
2025-10-05 15:12:56 -0500

    fix(database): fixes

--- src/backend/db/mongo.py
+++
@@ -1,6 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-import os
-
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
-client = AsyncIOMotorClient(MONGO_URL)
-db = client["smbsec"]
